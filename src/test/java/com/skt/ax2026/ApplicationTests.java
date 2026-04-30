f83b9ec2690cfe38c474320d78e2816e90b59691
CBJ\cbj52 <byungjun.choi@sk.com>
2026-05-01 01:02:40 +0900

    project init

---
+++ src/test/java/com/skt/ax2026/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skt.ax2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
